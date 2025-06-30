95a167230df5edd4486f409b906ceada0b5c091a
TylerThomas <tyler.thms@gmail.com>
2025-06-30 20:29:25 +0200

    add endpoint to get age ranges

---
+++ app/api/age_ranges/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  // This route is used to fetch age ranges for the platform user form
+  try {
+    const ageRanges = await prisma.ageRanges.findMany({
+      orderBy: { id: "asc" },
+    });
+
+    return NextResponse.json(ageRanges, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching age ranges:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch age ranges" },
+      { status: 500 }
+    );
+  }
+}
